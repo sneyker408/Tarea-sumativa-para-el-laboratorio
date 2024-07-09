97f873d3131a38447bd9983d804188d692c63e55
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2024-07-08 21:24:11 -0600

    Crear el modelo, creacion de la lista para recibir los valores de la tabla customers, crear un objeto de instancia y cast para evitar los DBNull  y colocarlos en el formatoque dicta la tabla 08-07-2024

--- ConexionEjemplo/Modelos/Customers.cs
+++ ConexionEjemplo/Modelos/Customers.cs
@@ -8,13 +8,14 @@ namespace ConexionEjemplo.Modelos
 {
     internal class Customers
     {
+
         public String CompanyName { get; set; } 
         public String ContactName { get; set;}
         public String ContactTitle { get; set;}
         public String Address { get; set;}
         public String City { get; set;}
         public String Region { get; set;}
-        public int  PostalCode { get; set;}
+        public String  PostalCode { get; set;}
         public String Country { get; set;}
         public String Phone { get; set;}
         public String Fax { get; set;}
