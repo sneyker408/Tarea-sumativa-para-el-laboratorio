2cd41d9dd11c1aea0940a3a27a6a88ad6a9d5481
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2024-07-08 23:52:15 -0600

    Guia X se añade un textbox y un label para filtrar las consultas que se realizaran desde la lista customer y se pasaran al dataGridView

--- ConexionEjemplo/Form1.cs
+++ ConexionEjemplo/Form1.cs
@@ -14,6 +14,9 @@ namespace ConexionEjemplo
 {
     public partial class Form1 : Form
     {
+
+        List<Customers> Customers = new List<Customers>();
+
         public Form1()
         {
             InitializeComponent();
@@ -48,7 +51,7 @@ namespace ConexionEjemplo
             SqlCommand comando = new SqlCommand(selectFrom, conexion);
             SqlDataReader reader = comando.ExecuteReader();
 
-            List < Customers > Customers = new List < Customers >();
+            
 
           while(reader.Read()){
 
@@ -66,9 +69,17 @@ namespace ConexionEjemplo
                 
                 Customers.Add(customers);
             }
-            
+            dataGrid.DataSource = Customers;
+
+
             MessageBox.Show("Conexion cerrada");
             conexion.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            var filtro = Customers.FindAll( X => X.CompanyName.StartsWith(tbFiltro.Text));
+            dataGrid.DataSource = filtro;
         }
     }
 }
